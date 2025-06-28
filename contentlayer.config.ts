7dbc4a0f2eface689faa55dbd3166d0b9496c308
Pawitra Warda <pawitrawarda@gmail.com>
2025-06-29 00:12:18 +0700

    config content layer for blog post

---
+++ contentlayer.config.ts
@@ -0,0 +1,100 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import readingTime from "reading-time";
+import GithubSlugger from "github-slugger";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrettyCode from "rehype-pretty-code";
+
+const Post = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: "**/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    publishedAt: {
+      type: "date",
+      required: true,
+    },
+    updatedAt: {
+      type: "date",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    image: {
+      type: "image",
+    },
+    isPublished: {
+      type: "boolean",
+      default: true,
+    },
+    author: {
+      type: "string",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      // eslint-disable-next-line no-underscore-dangle
+      resolve: (post) => `/blogs/${post._raw.flattenedPath}`,
+    },
+    readingTime: {
+      type: "json",
+      resolve: (doc) => readingTime(doc.body.raw),
+    },
+    toc: {
+      type: "json",
+      resolve: async (doc) => {
+        const regExp = /\n(?<flag>#{1,6})\s+(?<content>.+)/g;
+        const headings = Array.from(doc.body.raw.matchAll(regExp)).map(
+          ({ groups }: any) => {
+            const flag = groups?.flag;
+            const content = groups?.content;
+
+            const slugger = new GithubSlugger();
+            return {
+              level: (() => {
+                if (flag?.length === 1) return "one";
+                if (flag?.length === 2) return "two";
+
+                return "three";
+              })(),
+              text: content,
+              slug: content ? slugger.slug(content) : undefined,
+            };
+          },
+        );
+        return headings;
+      },
+    },
+  },
+}));
+
+const codeOptions = {
+  theme: "github-dark",
+};
+
+export default makeSource({
+  /* options */
+  contentDirPath: "articles",
+  documentTypes: [Post],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, { behavior: "append" }],
+      [rehypePrettyCode, codeOptions],
+    ] as any,
+  },
+});
---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,160 @@
+import Image from "next/image";
+import Toc, { IToc } from "@/app/blog/components/Toc";
+
+const toc: Array<IToc> = [
+  {
+    level: "two",
+    text: "What to expect from here on out",
+    slug: "what-to-expect-from-here-on-out",
+  },
+  {
+    level: "three",
+    text: "Typography should be easy",
+    slug: "typography-should-be-easy",
+  },
+  {
+    level: "two",
+    text: "What if we stack headings?",
+    slug: "what-if-we-stack-headings",
+  },
+  {
+    level: "three",
+    text: "We should make sure that looks good, too.",
+    slug: "we-should-make-sure-that-looks-good-too",
+  },
+  {
+    level: "three",
+    text: "When a heading comes after a paragraph …",
+    slug: "when-a-heading-comes-after-a-paragraph-",
+  },
+  {
+    level: "two",
+    text: "Code should look okay by default.",
+    slug: "code-should-look-okay-by-default",
+  },
+  {
+    level: "three",
+    text: "What about nested lists?",
+    slug: "what-about-nested-lists",
+  },
+  {
+    level: "two",
+    text: "There are other elements we need to style",
+    slug: "there-are-other-elements-we-need-to-style",
+  },
+  {
+    level: "three",
+    text: "Sometimes I even use `code` in headings",
+    slug: "sometimes-i-even-use-code-in-headings",
+  },
+  {
+    level: "three",
+    text: "We haven't used an `h4` yet",
+    slug: "we-havent-used-an-h4-yet",
+  },
+  {
+    level: "three",
+    text: "We still need to think about stacked headings though.",
+    slug: "we-still-need-to-think-about-stacked-headings-though",
+  },
+  {
+    level: "three",
+    text: "Let's make sure we don't screw that up with `h4` elements, either.",
+    slug: "lets-make-sure-we-dont-screw-that-up-with-h4-elements-either",
+  },
+  {
+    level: "two",
+    text: "GitHub Flavored Markdown",
+    slug: "github-flavored-markdown",
+  },
+];
+
+interface PageParams {
+  slug: string;
+}
+
+interface BlogPageProps {
+  params: PageParams;
+}
+
+export default function BlogPage({ params }: BlogPageProps) {
+  return (
+    <main className="pb-5 min-h-screen mx-auto dark:bg-black">
+      <article className="flex flex-col lg:flex-row lg:flex-wrap lg:mx-5">
+        <header className="lg:w-full flex flex-col m-3 border-b border-black pb-5 dark:border-[#FFF5EE]">
+          <figure className="relative h-[200px] md:h-[360px] mx-1 my-3 rounded-3xl overflow-hidden">
+            <Image
+              className="object-cover"
+              src="/dummy2.webp"
+              alt=""
+              fill
+              priority
+              sizes="(min-width: 1024px) 33vw, (min-width: 768px) 50vw, 100vw"
+            />
+          </figure>
+          <h1 className="text-3xl font-semibold mt-3 dark:text-[#FFF59F]">
+            Blog Title {params.slug}
+          </h1>
+          <span className="text-sm font-semibold font-[#2F393F] mt-1 dark:text-[#75FBC0]">
+            Posted on <time dateTime="2025-08-12">12/08/2025</time>
+          </span>
+        </header>
+        <Toc dataToc={toc} />
+        <div className="lg:flex-1 dark:text-[#FFF5EE]">
+          <section className="mt-5 px-3">
+            <h2 className="font-semibold text-lg">Lorem Ipsum Dolor</h2>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit.
+            </p>
+          </section>
+          <section className="mt-5 px-3">
+            <h2 className="font-semibold text-lg">Ut Enim Ad Minimh</h2>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.
+              Nullam varius, turpis et commodo pharetra, est eros bibendum elit.
+            </p>
+          </section>
+          <section className="mt-5 px-3">
+            <h2 className="font-semibold text-lg">Ut Enim Ad Minimh</h2>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.
+              Nullam varius, turpis et commodo pharetra, est eros bibendum elit.
+            </p>
+          </section>
+          <section className="mt-5 px-3">
+            <h2 className="font-semibold text-lg">Ut Enim Ad Minimh</h2>
+            <p className="text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.
+              Nullam varius, turpis et commodo pharetra, est eros bibendum elit.
+            </p>
+          </section>
+        </div>
+      </article>
+    </main>
+  );
+}
