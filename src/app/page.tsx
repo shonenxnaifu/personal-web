c05d6e1c0941983070a98d6773d9a95b06a74dbd
Pawitra Warda <pawitrawarda@gmail.com>
2025-09-04 15:57:58 +0700

    deploy to production

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import HomePage from "./home/HomePage";
+import CommingSoon from "./comming-soon/CommingSoon";
+// import HomePage from "./home/HomePage";
 
 export default function Home() {
-  return <HomePage />;
+  // return <HomePage />;
+  return <CommingSoon />;
 }
